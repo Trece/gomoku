484342cf09b62f588bfe3c5821cb17079772c797
Trece <treathen@hotmail.com>
2016-05-20 21:20:48 +0800

    dsf

--- value.py
+++ value.py
@@ -151,9 +151,9 @@ class ConvNetwork:
                 }
             )
         
-        layer3_input = theano.function(
+        layer3_output = theano.function(
             [index],
-            self.layer3.input,
+            self.layer3.output,
             givens={
                 x:train_set_x[index * batch_size: (index + 1) * batch_size]
                 }
@@ -198,7 +198,7 @@ class ConvNetwork:
                 if iter % 100 == 0:
                     print('training @ iter = ', iter)
                     print('cost = {}'.format(cost_ij))
-                    print('b = {}'.format(layer3_input(minibatch_index)))
+                    print('b = {}'.format(layer3_output(minibatch_index).sum()))
                     print('prediction is {}'.format(prediction(minibatch_index)))
                     print('', flush=True)
                 if (iter + 1) % validation_frequency == 0:
