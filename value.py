3fc3eadc90167bbc4fa4f0935c91eb94dfd55464
Trece <treathen@hotmail.com>
2016-05-20 21:38:21 +0800

    dsf

--- value.py
+++ value.py
@@ -199,7 +199,7 @@ class ConvNetwork:
                 if iter % 100 == 0:
                     print('training @ iter = ', iter)
                     print('cost = {}'.format(cost_ij))
-                    print('b = {}'.format(layer3_output(minibatch_index).sum()))
+                    print('b = {}'.format(network.layer3.W.get_value().sum()))
                     print('prediction is {}'.format(prediction(minibatch_index)))
                     print('', flush=True)
                 if (iter + 1) % validation_frequency == 0:
